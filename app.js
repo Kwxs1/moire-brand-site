2526922df4aac037ce81f3fa8d1443036cdd552e
Kwxs1 <134171545+Kwxs1@users.noreply.github.com>
2024-01-15 22:45:05 -0500

    All frameworks complete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu'); //var controlling drop down menu
 const menuLinks = document.querySelector('.navbar__menu'); //var controlling link buttons
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display Mobile Menu
 const mobileMenu = () => { //arrow function
@@ -8,3 +9,50 @@ const mobileMenu = () => { //arrow function
 };
 
 menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+
+const highlightMenu = () => {
+    const element = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const missionMenu = document.querySelector('#mission-page');
+    const designsMenu = document.querySelector('#designs-page');
+    let scrollPos = window.scrollY
+    console.log(scrollPos);
+
+    // add highlight class to menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        missionMenu.classList.remove('highlight');
+        return; 
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        homeMenu.classList.remove('highlight');
+        missionMenu.classList.add('highlight');
+        designsMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        designsMenu.classList.add('highlight');
+        missionMenu.classList.remove('highlight');
+        return;
+    }
+
+    if((element && window.innerWidth < 960 && scrollPos < 600) || element){
+        element.classList.remove('highlight');
+    }
+}
+
+window.addEventListener('scroll', highlightMenu)
+window.addEventListener('click', highlightMenu)
+
+
+//close hamburger menu after clicking on item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector(".is-active") 
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
